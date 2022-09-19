b7a340c7fd27e32976bd8228c414e293fc53198c
yaman-694 <jyaman694@gmail.com>
2022-09-19 12:20:10 +0530

    test

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        print(file)
+        return {"ok": True}
+
+
+if __name__ == '__main__':
+    app.run()
