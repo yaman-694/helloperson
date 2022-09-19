acd4fe24265be409760f3bea28e222660d7faad5
yaman-694 <jyaman694@gmail.com>
2022-09-19 12:02:41 +0530

    test

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-import numpy, requests
 from flask import Flask, request, jsonify
-import joblib
 
 app = Flask(__name__)
 
@@ -18,17 +16,5 @@ def predict():
         return {"ok": True}
 
 
-@app.route('/test', methods=['GET'])
-def send_audio():
-    url = 'http://127.0.0.1:5000/predict'
-    with open("D:/UrbanSound8K/audio/fold3/6988-5-0-2.wav", 'rb') as file:
-        data = {'uuid': '-jx-1', 'alarmType': 1, 'timeDuration': 10}
-        files = {'messageFile': file}
-        req = requests.post(url, files=files, json=data)
-        print(req.status_code)
-        print(req.text)
-    return 'some thing'
-
-
 if __name__ == '__main__':
-    app.run(host='192.168.1.44', port=8000)
+    app.run()
